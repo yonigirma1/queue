fccfd297f01a095fa965fbf407550af2338d8583
Yonatan Girma <ygirma.exclusive@yahoo.com>
2018-01-13 21:15:19 -0600

    Queue implementaion in Python

---
+++ queue.py
@@ -0,0 +1,18 @@
+#Implementation of Queue in Python
+
+class Queue(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self,item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def sizeOf(self):
+        return len(self.items)
